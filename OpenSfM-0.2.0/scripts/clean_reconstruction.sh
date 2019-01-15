9579c13685ada01d535681af619b1de06f67338e
Ubuntu <ubuntu@ip-172-31-32-224.us-east-2.compute.internal>
2019-01-15 22:56:00 +0000

    Bash script to clean a reconstruction

---
+++ OpenSfM-0.2.0/scripts/clean_reconstruction.sh
@@ -0,0 +1,29 @@
+dataset=$1
+
+rm -R $dataset/all_matches
+rm -R $dataset/camera_models.json
+rm -R $dataset/classifier_dataset
+rm -R $dataset/classifier_features
+rm -R $dataset/config.yaml
+#rm -R $dataset/dslr_calibration_undistorted
+rm -R $dataset/exif
+rm -R $dataset/features
+#rm -R $dataset/images
+rm -R $dataset/matches
+rm -R $dataset/output.log
+rm -R $dataset/pairwise_results
+rm -R $dataset/profile.log
+rm -R $dataset/reconstruction_colmap.json
+#rm -R $dataset/reconstruction_gt.json
+rm -R $dataset/reconstruction.json
+rm -R $dataset/reference_lla.json
+rm -R $dataset/reports
+rm -R $dataset/results
+rm -R $dataset/sift
+#rm -R $dataset/sparse_converted
+rm -R $dataset/tracks-all-matches.csv
+rm -R $dataset/tracks.csv
+rm -R $dataset/tracks-thresholded-matches.csv
+rm -R $dataset/vocab_out
+rm -R $dataset/vt_image_list.txt
+rm -R $dataset/vt_sift_list.txt
