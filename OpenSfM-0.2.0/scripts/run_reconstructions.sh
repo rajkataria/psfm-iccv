625a977b6b793c0829660e942703dae0c6f38bfb
rk2 <rk2@illinois.edu>
2019-02-13 21:11:26 -0600

    Updating run reconstruction scripts since weighted resectioning isn't quite working yet

--- OpenSfM-0.2.0/scripts/run_reconstructions.sh
+++ OpenSfM-0.2.0/scripts/run_reconstructions.sh
@@ -91,62 +91,62 @@ echo "**************************************************************************
 
 ./bin/opensfm reconstruct $dataset
 
-# Using ground-truth matches and weighted resectioning - thresholding won't matter (since the weight is either a 1 or a 0)
-sed -i 's/use_gt_matches: false/use_gt_matches: true/g' $dataset/config.yaml
-sed -i 's/use_image_matching_classifier: false/use_image_matching_classifier: true/g' $dataset/config.yaml
-sed -i 's/use_weighted_resectioning: false/use_weighted_resectioning: true/g' $dataset/config.yaml
-sed -i 's/use_weighted_feature_matches: true/use_weighted_feature_matches: false/g' $dataset/config.yaml
-sed -i 's/use_image_matching_thresholding: true/use_image_matching_thresholding: false/g' $dataset/config.yaml
-echo "************************************************************************************************************"
-echo "************************************************************************************************************"
-echo "Classifier configurations:"
-grep "use_image_matching_classifier" $dataset/config.yaml
-grep "use_weighted_resectioning" $dataset/config.yaml
-grep "use_gt_matches" $dataset/config.yaml
-grep "use_weighted_feature_matches" $dataset/config.yaml
-grep "use_image_matching_thresholding" $dataset/config.yaml
-echo "************************************************************************************************************"
-echo "************************************************************************************************************"
+# # Using ground-truth matches and weighted resectioning - thresholding won't matter (since the weight is either a 1 or a 0)
+# sed -i 's/use_gt_matches: false/use_gt_matches: true/g' $dataset/config.yaml
+# sed -i 's/use_image_matching_classifier: false/use_image_matching_classifier: true/g' $dataset/config.yaml
+# sed -i 's/use_weighted_resectioning: false/use_weighted_resectioning: true/g' $dataset/config.yaml
+# sed -i 's/use_weighted_feature_matches: true/use_weighted_feature_matches: false/g' $dataset/config.yaml
+# sed -i 's/use_image_matching_thresholding: true/use_image_matching_thresholding: false/g' $dataset/config.yaml
+# echo "************************************************************************************************************"
+# echo "************************************************************************************************************"
+# echo "Classifier configurations:"
+# grep "use_image_matching_classifier" $dataset/config.yaml
+# grep "use_weighted_resectioning" $dataset/config.yaml
+# grep "use_gt_matches" $dataset/config.yaml
+# grep "use_weighted_feature_matches" $dataset/config.yaml
+# grep "use_image_matching_thresholding" $dataset/config.yaml
+# echo "************************************************************************************************************"
+# echo "************************************************************************************************************"
 
-./bin/opensfm reconstruct $dataset
+# ./bin/opensfm reconstruct $dataset
 
-# Use image matching classifier and weighted RANSAC using feature matching weights - threshold image classifier weights
-sed -i 's/use_gt_matches: true/use_gt_matches: false/g' $dataset/config.yaml
-sed -i 's/use_image_matching_classifier: false/use_image_matching_classifier: true/g' $dataset/config.yaml
-sed -i 's/use_weighted_resectioning: true/use_weighted_resectioning: false/g' $dataset/config.yaml
-sed -i 's/use_weighted_feature_matches: false/use_weighted_feature_matches: true/g' $dataset/config.yaml
-sed -i 's/use_image_matching_thresholding: false/use_image_matching_thresholding: true/g' $dataset/config.yaml
-echo "************************************************************************************************************"
-echo "************************************************************************************************************"
-echo "Classifier configurations:"
-grep "use_image_matching_classifier" $dataset/config.yaml
-grep "use_weighted_resectioning" $dataset/config.yaml
-grep "use_gt_matches" $dataset/config.yaml
-grep "use_weighted_feature_matches" $dataset/config.yaml
-grep "use_image_matching_thresholding" $dataset/config.yaml
-echo "************************************************************************************************************"
-echo "************************************************************************************************************"
+# # Use image matching classifier and weighted RANSAC using feature matching weights - threshold image classifier weights
+# sed -i 's/use_gt_matches: true/use_gt_matches: false/g' $dataset/config.yaml
+# sed -i 's/use_image_matching_classifier: false/use_image_matching_classifier: true/g' $dataset/config.yaml
+# sed -i 's/use_weighted_resectioning: true/use_weighted_resectioning: false/g' $dataset/config.yaml
+# sed -i 's/use_weighted_feature_matches: false/use_weighted_feature_matches: true/g' $dataset/config.yaml
+# sed -i 's/use_image_matching_thresholding: false/use_image_matching_thresholding: true/g' $dataset/config.yaml
+# echo "************************************************************************************************************"
+# echo "************************************************************************************************************"
+# echo "Classifier configurations:"
+# grep "use_image_matching_classifier" $dataset/config.yaml
+# grep "use_weighted_resectioning" $dataset/config.yaml
+# grep "use_gt_matches" $dataset/config.yaml
+# grep "use_weighted_feature_matches" $dataset/config.yaml
+# grep "use_image_matching_thresholding" $dataset/config.yaml
+# echo "************************************************************************************************************"
+# echo "************************************************************************************************************"
 
-./bin/opensfm reconstruct $dataset
+# ./bin/opensfm reconstruct $dataset
 
-# Use image matching classifier and weighted RANSAC using feature matching weights and weighted resectioning (don't threshold matches)
-sed -i 's/use_gt_matches: true/use_gt_matches: false/g' $dataset/config.yaml
-sed -i 's/use_image_matching_classifier: false/use_image_matching_classifier: true/g' $dataset/config.yaml
-sed -i 's/use_weighted_resectioning: false/use_weighted_resectioning: true/g' $dataset/config.yaml
-sed -i 's/use_weighted_feature_matches: false/use_weighted_feature_matches: true/g' $dataset/config.yaml
-sed -i 's/use_image_matching_thresholding: true/use_image_matching_thresholding: false/g' $dataset/config.yaml
-echo "************************************************************************************************************"
-echo "************************************************************************************************************"
-echo "Classifier configurations:"
-grep "use_image_matching_classifier" $dataset/config.yaml
-grep "use_weighted_resectioning" $dataset/config.yaml
-grep "use_gt_matches" $dataset/config.yaml
-grep "use_weighted_feature_matches" $dataset/config.yaml
-grep "use_image_matching_thresholding" $dataset/config.yaml
-echo "************************************************************************************************************"
-echo "************************************************************************************************************"
+# # Use image matching classifier and weighted RANSAC using feature matching weights and weighted resectioning (don't threshold matches)
+# sed -i 's/use_gt_matches: true/use_gt_matches: false/g' $dataset/config.yaml
+# sed -i 's/use_image_matching_classifier: false/use_image_matching_classifier: true/g' $dataset/config.yaml
+# sed -i 's/use_weighted_resectioning: false/use_weighted_resectioning: true/g' $dataset/config.yaml
+# sed -i 's/use_weighted_feature_matches: false/use_weighted_feature_matches: true/g' $dataset/config.yaml
+# sed -i 's/use_image_matching_thresholding: true/use_image_matching_thresholding: false/g' $dataset/config.yaml
+# echo "************************************************************************************************************"
+# echo "************************************************************************************************************"
+# echo "Classifier configurations:"
+# grep "use_image_matching_classifier" $dataset/config.yaml
+# grep "use_weighted_resectioning" $dataset/config.yaml
+# grep "use_gt_matches" $dataset/config.yaml
+# grep "use_weighted_feature_matches" $dataset/config.yaml
+# grep "use_image_matching_thresholding" $dataset/config.yaml
+# echo "************************************************************************************************************"
+# echo "************************************************************************************************************"
 
-./bin/opensfm reconstruct $dataset
+# ./bin/opensfm reconstruct $dataset
 
 ./bin/opensfm convert_colmap $dataset
 ./bin/opensfm validate_results $dataset
--- scripts/run_classifiers.sh
+++ scripts/run_classifiers.sh
@@ -6,8 +6,6 @@ declare -a classifier=("NN" "GCN")
 declare -a use_small_weights=("yes" "no")
 declare -a use_all_training_data=("yes" "no")
 
-# declare inputs_folder="scoring-inputs-12-09-2018-11-05-pm"
-
 for n in "${n_estimators[@]}"
 do
 	for d in "${max_depth[@]}"
@@ -33,22 +31,6 @@ do
 								--use_small_weights $s \
 								--use_all_training_data $a \
 								2>&1 > output-$c-use-all-training-data-$a-use-small-weights-$s-min-match-$m-max-match-$x.out &
-							# nohup python -u calculate_scoring_matrix.py \
-							# 	-u ./$inputs_folder/Users.txt \
-							# 	-r ./$inputs_folder/reviewers.csv \
-							# 	-t ./$inputs_folder/ReviewerTpmsScores_CVPR2019.csv \
-							# 	-p ./$inputs_folder/Papers.csv \
-							# 	-q ./$inputs_folder/quotas.csv \
-							# 	-s ./$inputs_folder/ReviewerSuggestions.txt \
-							# 	-c ./$inputs_folder/ReviewerConflicts.txt \
-							# 	-n 3 \
-							# 	-g $g \
-							# 	-w_t $t \
-							# 	-w_a $a \
-							# 	-w_s $s \
-							# 	-w_e $e \
-							# 	-o $c \
-							# 	--cached_folder ./output-w_t-$t-w_a-$a-w_s-$s-w_e-$e-g-$g-n-3-config-$c/ > output-w_t-$t-w_a-$a-w_s-$s-w_e-$e-g-$g-n-3-config-$c.log 2>&1 &
 							echo "Running '$c' classifier with the options use_all_training_data=$a image_match_classifier_min_match=$m image_match_classifier_max_match=$x use_small_weights=$s";
 						done;
 					done;
