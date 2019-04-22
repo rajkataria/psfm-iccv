395e824e026180f2ba8df249d48bc8e39a371063
rk2 <rk2@illinois.edu>
2019-04-22 09:56:08 -0500

    Renaming opensfm command

--- OpenSfM-0.2.0/opensfm/commands/convert_nvm.py
+++ OpenSfM-0.2.0/opensfm/commands/convert_nvm.py
@@ -12,8 +12,8 @@ from opensfm import io
 logger = logging.getLogger(__name__)
 
 class Command:
-    name = 'convert_colmap'
-    help = "Convert colmap output(NVM) to reconstruction"
+    name = 'convert_nvm'
+    help = "Convert NVMs (colmap/theia) to reconstruction"
 
     def add_arguments(self, parser):
         parser.add_argument('dataset', help='dataset to process')
@@ -21,9 +21,11 @@ class Command:
     def run(self, args):
         data = dataset.DataSet(args.dataset)
         colmap_fn = os.path.join(data.data_path, 'sparse_converted', 'colmap-output.nvm')
-        reconstruction_fn = os.path.join(data.data_path, 'reconstruction_colmap.json')
-        logger.info('Converting "{}" to "{}":'.format(os.path.basename(colmap_fn), os.path.basename(reconstruction_fn)))
-        io.reconstruction_from_nvm(data.data_path, colmap_fn, reconstruction_fn)
+        theia_fn = os.path.join(data.data_path, 'theia', 'theia-output.nvm')
+        for fn, label in [[colmap_fn, 'colmap'], [theia_fn, 'theia']]:
+            reconstruction_fn = os.path.join(data.data_path, 'reconstruction_{}.json'.format(label))
+            logger.info('Converting "{}" to "{}":'.format(os.path.basename(fn), os.path.basename(reconstruction_fn)))
+            io.reconstruction_from_nvm(data.data_path, fn, reconstruction_fn)
         
 
 
