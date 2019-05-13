0a33357ce1a4488ea18cff5462821da0c1b78c45
Ubuntu <ubuntu@ip-172-31-5-69.us-east-2.compute.internal>
2019-05-13 14:01:32 +0000

    Minor updates

--- OpenSfM-0.2.0/scripts/run_eth3d.sh
+++ OpenSfM-0.2.0/scripts/run_eth3d.sh
@@ -1,4 +1,4 @@
 for i in /home/ubuntu/Results/classifier/ETH3D/*/;
 do
-    ./bin/opensfm_create_classifier_datasets $i
+    ./bin/opensfm_create_classifier_datasets $i > $i/output.log 2&>1
 done
--- OpenSfM-0.2.0/scripts/run_tum_rgbd_slam.sh
+++ OpenSfM-0.2.0/scripts/run_tum_rgbd_slam.sh
@@ -1,4 +1,4 @@
 for i in /home/ubuntu/Results/classifier/TUM_RGBD_SLAM/*/;
 do
-    ./bin/opensfm_create_classifier_datasets $i
+    ./bin/opensfm_create_classifier_datasets $i > $i/output.log 2&>1
 done
