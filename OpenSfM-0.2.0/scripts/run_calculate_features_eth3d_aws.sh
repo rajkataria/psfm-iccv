657429d615e89ebd5ed90c5ac7bfcb37abed52df
rk2 <rk2@illinois.edu>
2019-07-23 10:38:51 -0500

    Outputting extra info

--- OpenSfM-0.2.0/scripts/run_calculate_features_eth3d_aws.sh
+++ OpenSfM-0.2.0/scripts/run_calculate_features_eth3d_aws.sh
@@ -1,4 +1,6 @@
 for i in /home/ubuntu/Results/completed-classifier-datasets-bruteforce/ETH3D/*/;
 do
+    echo "***************************************************************************************************"
+    echo $i
     ./bin/opensfm calculate_features $i
 done
--- OpenSfM-0.2.0/scripts/run_calculate_features_tanksandtemples_aws.sh
+++ OpenSfM-0.2.0/scripts/run_calculate_features_tanksandtemples_aws.sh
@@ -1,4 +1,6 @@
 for i in /home/ubuntu/Results/completed-classifier-datasets-bruteforce/TanksAndTemples/*/;
 do
+    echo "***************************************************************************************************"
+    echo $i
     ./bin/opensfm calculate_features $i 
 done
